11fd6e6ce9dc26ab1578d26bec5e70ed15cf9fb4
NickWheel <sungamma.99@gmail.com>
2019-05-20 13:50:18 +0300

    fatorial on WHILE cycle

---
+++ main.js
@@ -0,0 +1,56 @@
+// ---------------------read about it-----------------------------------------
+// let a = 5;
+// let b = (a > 4) ? 'bigger' : 'smaller';
+// console.log(b);
+
+
+
+// let acsess = +prompt('Skoka let >???????');
+
+// let age = (acsess > 18) ? 'proxodi' : 'NIZYA!!!';
+// console.log(age);
+
+
+
+// let age = +prompt('Skoka let >???????');
+
+// switch (age) {
+//     case 18:
+//         console.log('well ok then');
+//         break;
+//     default:
+//         console.log('GO');
+// }
+// let a = 5;
+// let a++ ;
+// console.log(a)
+
+
+
+// let array = ['kek', 'shrek', 'lol', 'serGAY)))']
+// console.log(array['shrek'])
+
+
+
+// for (let i = 0; i<10; i++) {
+//     if (i%2 === 0 && i!= 0)  {
+//         console.log(i);
+//     }
+// }
+
+
+
+value = +prompt('vvedi chislo kotoroe hochew voznesti do nebes');
+sum = 1;
+let fact_beginning=1
+
+while (fact_beginning <= value) {
+    sum *= fact_beginning;
+    fact_beginning++
+    console.log(sum);
+}
+
+
+
+
+// -----------------------------------------------------------------------------
